e45645fccb7fe352189a126e345578c5ea097d97
Daria Metelitsa <dariametelitsa@gmail.com>
2025-02-05 14:16:34 +0300

    feat: refetch with invalidateQueries in right place

--- src/modules/todo-list/useCreateTodo.ts
+++ src/modules/todo-list/useCreateTodo.ts
@@ -8,6 +8,8 @@ export const useCreateTodo = () => {
 
   const createTodo = useMutation({
     mutationFn: todoListApi.createTodo,
+    onSuccess: () =>
+      queryClient.invalidateQueries(todoListApi.getTodolistQueryOptions()),
   });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -15,13 +17,7 @@ export const useCreateTodo = () => {
     const formData = new FormData(e.currentTarget);
 
     const title = String(formData.get('title') ?? '');
-    createTodo.mutate(
-      { id: nanoid(), done: false, userId: '1', text: title },
-      {
-        onSuccess: () =>
-          queryClient.invalidateQueries({ queryKey: [todoListApi.baseKey] }),
-      }
-    );
+    createTodo.mutate({ id: nanoid(), done: false, userId: '1', text: title });
     e.currentTarget.reset();
   };
 
